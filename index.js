0584a13630c43b076ec882a151edbf835fbe9258
NandiniAccolite <nandini.katara@accolitedigital.com>
2024-03-12 09:35:25 +0530

    completed

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-$(document).ready(function () {
-    $("#addDetailsBtn").click(function () {
-        const firstName = $("#firstName").val();
-        const lastName = $("#lastName").val();
-        const age = $("#age").val();
-        const gender = $("#gender").val();
-        const rollNo = $("#rollNo").val();
-        const dob = $("#dob").val();
-        const hobbies = $("#hobbies").val();
-        if (firstName === "" || lastName === "" || age === "" || gender === "" || rollNo === "" || dob === "" || hobbies === "") {
-            alert("Please fill in all fields.");
-            return;
-        }
-        alert("Data added successfully !!")
-        let personsArray = [];
-        if (localStorage.getItem("studentDatabase")) {
-            personsArray = JSON.parse(localStorage.getItem("studentDatabase"));
-        }
-        let personDetails = { rollNo: rollNo, firstName: firstName, lastName: lastName, age: age, gender: gender, dob: dob, hobbies: hobbies };
-        personsArray.push(personDetails);
-        let detailString = JSON.stringify(personsArray);
-        localStorage.setItem("studentDatabase", detailString);
-    });
+function submitPersonDetails() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const age = document.getElementById("age").value;
+    const gender = document.getElementById("gender").value;
+    const rollNo = document.getElementById("rollNo").value;
+    const dob=document.getElementById("dob").value;
+    const hobbies=document.getElementById("hobbies").value;
+    if (firstName === "" || lastName === "" || age === "" || gender === "" || rollNo === "" || dob==="" || hobbies==="") {
+alert("Please fill in all fields.");
+return;
+}
+alert("Details added Successfully !!")
+    // const personDetails = {
+    //     firstName: firstName,
+    //     lastName: lastName,
+    //     age: age,
+    //     gender: gender,
+    //     rollNo: rollNo
+    // };
+    let personsArray = [];
+    if(localStorage.getItem("studentDatabase")){
+        personsArray = JSON.parse(localStorage.getItem("studentDatabase"));
+    }
+    let personDetails = { rollNo: rollNo, firstName: firstName, lastName: lastName, age: age, gender: gender, dob:dob ,hobbies:hobbies };
+    personsArray.push(personDetails);
+    let detailString = JSON.stringify(personsArray);
 
-    $("#getDetailsBtn").click(function (event) {
-        event.preventDefault();
-        const rollToSearch = $("#roll").val();
-        let personsArray = [];
-        if (localStorage.getItem("studentDatabase")) {
-            personsArray = JSON.parse(localStorage.getItem("studentDatabase"));
-        }
+    localStorage.setItem("studentDatabase", detailString);
+}
 
-        const foundPerson = personsArray.find(person => person.rollNo == rollToSearch);
-        const detailsContent = $("#detailsContent");
-        if (foundPerson) {
-            detailsContent.html(`
-                <strong>Roll Number:</strong> ${foundPerson.rollNo}<br>
-                <strong>First Name:</strong> ${foundPerson.firstName}<br>
-                <strong>Last Name:</strong> ${foundPerson.lastName}<br>
-                <strong>Age:</strong> ${foundPerson.age}<br>
-                <strong>Gender:</strong> ${foundPerson.gender}<br>
-                <strong>Date of Birth:</strong> ${foundPerson.dob}<br>
-                <strong>Hoobies:</strong> ${foundPerson.hobbies}<br>
-            `);
-        } else {
-            detailsContent.html("Student not found with this Roll Number.");
-        }
-    });
-});
+function displayDetails(event) {
+    event.preventDefault(); 
+    const rollToSearch = document.getElementById("roll").value;
+    let personsArray = [];
+    if(localStorage.getItem("studentDatabase")){
+        personsArray = JSON.parse(localStorage.getItem("studentDatabase"));
+    }
+
+    console.log("recieved value ", personsArray)
+
+    const foundPerson = personsArray.find(person => person.rollNo == rollToSearch);
+    console.log("found person is", foundPerson)
+    const detailsContent = document.getElementById("detailsContent");
+    const myDetails = document.getElementById("myDetails");
+    if (foundPerson) {
+        detailsContent.innerHTML = `
+            <strong>Roll Number:</strong> ${foundPerson.rollNo}<br>
+            <strong>First Name:</strong> ${foundPerson.firstName}<br>
+            <strong>Last Name:</strong> ${foundPerson.lastName}<br>
+            <strong>Age:</strong> ${foundPerson.age}<br>
+            <strong>Gender:</strong> ${foundPerson.gender}<br>
+            <strong>Date of Birth:</strong> ${foundPerson.dob}<br>
+            <strong>Hoobies:</strong> ${foundPerson.hobbies}<br>
+
+        `;
+    } else {
+        detailsContent.innerHTML = "Student not found with the this Roll Number.";
+    }
+}
